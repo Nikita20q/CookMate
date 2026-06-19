d7895a953035cafc7876171b3b5d53f996503b59
mariance <nicitaprotsencko@yandex.ru>
2026-06-19 13:57:44 +0300

    feat created API

--- Gateway/build.gradle.kts
+++ Gateway/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.4.2"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "org.example"
@@ -9,10 +11,18 @@ repositories {
     mavenCentral()
 }
 
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2024.0.0")
+    }
+}
+
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway")
 }
 
 tasks.test {
