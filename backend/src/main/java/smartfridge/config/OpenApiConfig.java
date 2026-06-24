7f594140d65b4a3c6b3d4765abec0e2c8a9fc895
mariance <nicitaprotsencko@yandex.ru>
2026-06-24 10:50:49 +0300

    fix: fix server

--- backend/src/main/java/smartfridge/config/OpenApiConfig.java
+++ backend/src/main/java/smartfridge/config/OpenApiConfig.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +14,9 @@ import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
+
+    @Value("${app.server-url:http://localhost:8080}")
+    private String serverUrl;
 
     @Bean
     public OpenAPI customOpenAPI() {
@@ -25,8 +29,8 @@ public class OpenApiConfig {
                         .description("API для управления рецептами и избранным"))
                 .servers(List.of(
                         new Server()
-                                .url("http://localhost:8080")
-                                .description("Local server")
+                                .url(serverUrl)
+                                .description("Backend server")
                 ))
                 .addSecurityItem(new SecurityRequirement()
                         .addList(securitySchemeName))
